fd0d806b94cbe508af5d61b8126674186c7b0db5
Bill Marques <pnxbill@gmail.com>
2020-07-19 23:47:23 -0300

    feat: Added navigation to whole app with link from next/link

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,38 +1,37 @@
 
-import { Navbar, Navdropdown, Nav } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
+import Link from 'next/link';
 
+const AppLink = ({ children, className, href }) =>
+  <Link href={href}>
+    <a className={`mr-3 ${className || ""}`}>{children}</a>
+  </Link>
 
 const AppNavbar = () => {
 
   return (
     <div className="navbar-wrapper">
       <Navbar expand="lg" className="navbar-dark fj-mw9">
-        <Navbar.Brand className="mr-3 font-weight-bold" href="#">
+
+        <AppLink href="/" className="navbar-brand font-weight-bold">
           Bill Marques
-        </Navbar.Brand>
+        </AppLink>
+
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className="mr-auto">
-            <Nav.Link className="mr-3" href="#" >
-              Portfolios
-            </Nav.Link>
-            <Nav.Link className="mr-3" href="#">
-              Forum
-            </Nav.Link>
-            <Nav.Link className="mr-3" href="#">
-              Cv
-            </Nav.Link>
-            <Nav.Link className="mr-3" href="#">
-              Ask me
-            </Nav.Link>
+            <AppLink href="/portfolios" className="nav-link">Portfolios</AppLink>
+            <AppLink href="/forum/categories" className="nav-link">Forum</AppLink>
+            <AppLink href="/cv" className="nav-link">CV</AppLink>
           </Nav>
           <Nav>
-            <Nav.Link className="mr-3" href="#">
+            <AppLink href="/login" className="nav-link">Sign In</AppLink>
+            <AppLink
+              href="/register"
+              className="nav-link btn btn-success bg-green-2 bright"
+            >
               Sign Up
-            </Nav.Link>
-            <Nav.Link className="mr-3 btn btn-success bg-green-2 bright" href="#">
-              Sign In
-            </Nav.Link>
+            </AppLink>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
