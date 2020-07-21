d3b4e2b0aa453d1209c2685a49a677677994447c
Bill Marques <pnxbill@gmail.com>
2020-07-20 22:49:17 -0300

    chore: Testing queries and types inside server

--- server/index.js
+++ server/index.js
@@ -7,21 +7,64 @@ const { buildSchema } = require('graphql');
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
-const handle = app.getRequestHandler()
+const handle = app.getRequestHandler();
+
+const data = {
+  portfolios: [
+    {
+      _id: '312id2312',
+      title: 'Job in USA',
+      content: 'It was a very nice experience',
+      jobTitle: 'Chef',
+      daysOfExperience: 100,
+      isCurrentlyEmployed: false
+    },
+    {
+      _id: '33fj21k3',
+      title: 'Job in Barcelona',
+      content: 'Barcelona rocks!',
+      jobTitle: 'Chef',
+      isCurrentlyEmployed: false
+    },
+    {
+      _id: '31k2j31l',
+      title: 'Job in Germany',
+      content: 'Dark is a terrible TV Show',
+      jobTitle: 'Manager',
+      daysOfExperience: 132,
+      isCurrentlyEmployed: true
+    },
+  ]
+};
 
 app.prepare().then(() => {
   const server = express();
   // Construct a scheema using GRAPHQL
   const schema = buildSchema(`
+    type Portfolio {
+      _id: ID!
+      title: String
+      content: String
+      jobTitle: String
+      daysOfExperience: Int
+      isCurrentlyEmployed: Boolean
+    }
+
     type Query {
       hello: String
+      portfolio: Portfolio
+      portfolios: [Portfolio]
     }
   `);
   // Provides a resolver for each graphql endpoint
   const root = {
     hello: () => {
       return 'Hello World'
-    }
+    },
+    portfolio: () => {
+      return data.portfolios[0]
+    },
+    portfolios: () => data.portfolios
   };
 
   server.use('/graphql', graphqlHTTP({
