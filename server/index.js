465c056447bb5a6411b020b9bea5341abc9eea3e
Bill Marques <pnxbill@gmail.com>
2020-07-20 22:24:43 -0300

    feat: Implemented graphql on the server with a simple query for testing

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 
 const express = require('express')
-const next = require('next')
+const next = require('next');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
@@ -8,7 +10,25 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  const server = express()
+  const server = express();
+  // Construct a scheema using GRAPHQL
+  const schema = buildSchema(`
+    type Query {
+      hello: String
+    }
+  `);
+  // Provides a resolver for each graphql endpoint
+  const root = {
+    hello: () => {
+      return 'Hello World'
+    }
+  };
+
+  server.use('/graphql', graphqlHTTP({
+    schema,
+    rootValue: root,
+    graphiql: true
+  }));
 
   server.all('*', (req, res) => {
     return handle(req, res)
