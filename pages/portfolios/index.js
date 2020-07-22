3b88b3cb3146cf74120b4d423faad2c0a09ed962
Bill Marques <pnxbill@gmail.com>
2020-07-21 23:09:07 -0300

    feat: iteration over portfolios comming from the backend and creation of a new component to handle portfolio card display

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,4 +1,5 @@
 import Axios from "axios";
+import PortfolioCard from "../../components/portfolios/PortfolioCard";
 
 const fetchPortfolios = () => {
   const query = `
@@ -11,6 +12,8 @@ const fetchPortfolios = () => {
           location
           jobTitle
           description  
+          startDate
+          endDate
         }
       }
     `;
@@ -29,47 +32,16 @@ const Portfolios = ({ portfolios }) => {
             <h1>Portfolios</h1>
           </div>
         </div>
-        {JSON.stringify(portfolios)}
-        <button onClick={fetchPortfolios} className="btn btn-primary">Fetch Data</button>
       </section>
       <section className="pb-5">
         <div className="row">
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-4">
-            <div className="card subtle-shadow no-border">
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              </div>
-              <div className="card-footer no-border">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </div>
-            </div>
-          </div>
+          {portfolios.map(portfolio => {
+            return (
+              <div key={portfolio._id} className="col-md-4">
+                <PortfolioCard portfolio={portfolio} />
+              </div>)
+          })}
+
         </div>
       </section>
     </>
