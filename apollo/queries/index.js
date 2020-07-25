7b602a094307dea716058c38cd3550d4639b9087
Bill Marques <pnxbill@gmail.com>
2020-07-25 16:43:58 -0300

    chore: Added Apollo client and apollo boost to our frontend

---
+++ apollo/queries/index.js
@@ -0,0 +1,34 @@
+import { gql } from 'apollo-boost';
+
+
+
+export const GET_PORTFOLIO = gql`
+  query Portfolio($id: ID) {
+      portfolio(id: $id) {
+        _id
+        title
+        jobTitle
+        description
+        company
+        companyWebsite
+        startDate
+        endDate
+      }
+    }
+`;
+
+export const GET_PORTFOLIOS = gql`
+  query Portfolios {
+    portfolios {
+      _id
+      title
+      company
+      companyWebsite
+      location
+      jobTitle
+      description  
+      startDate
+      endDate
+    }
+  }
+`;
