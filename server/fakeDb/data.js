a1c6143eadf312aef51b9ef81bfcacb60b4453f6
Bill Marques <pnxbill@gmail.com>
2020-07-27 21:06:24 -0300

    feat: populating DB with users and linking portfolios to users

--- server/fakeDb/data.js
+++ server/fakeDb/data.js
@@ -1,4 +1,30 @@
+const mongoose = require('mongoose');
+
+const user1Id = mongoose.Types.ObjectId();
+const user2Id = mongoose.Types.ObjectId();
+
 const data = {
+  users: [
+    {
+      _id: user1Id,
+      avatar: 'https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png',
+      email: "bill@gmail.com",
+      name: "Bill Marques",
+      username: "pnxbill",
+      info: "Hello I'm Bill",
+      password: "testtest",
+      role: "admin"
+    },
+    {
+      _id: user2Id,
+      avatar: 'https://www.shareicon.net/data/128x128/2017/01/06/868320_people_512x512.png',
+      email: "test@gmail.com",
+      name: "Test User",
+      username: "test99",
+      info: "Hello I'm Test",
+      password: "testtest",
+    },
+  ],
   portfolios: [
     {
       title: 'Job in Netcentric',
@@ -8,7 +34,8 @@ const data = {
       jobTitle: 'Engineer',
       description: 'Doing something, programing....',
       startDate: '01/01/2014',
-      endDate: '01/01/2016'
+      endDate: '01/01/2016',
+      user: user1Id
     },
     {
       title: "Job in Siemens",
@@ -18,7 +45,8 @@ const data = {
       jobTitle: "Software Engineer",
       description: "Responsoble for parsing framework for JSON medical data.",
       startDate: "01/01/2011",
-      endDate: "01/01/2013"
+      endDate: "01/01/2013",
+      user: user1Id
     },
     {
       title: 'Work in USA',
@@ -28,7 +56,8 @@ const data = {
       jobTitle: 'Housekeeping',
       description: 'So much responsibility....Overloaaaaaad',
       startDate: '01/01/2010',
-      endDate: '01/01/2011'
+      endDate: '01/01/2011',
+      user: user1Id
     }
   ]
 };
