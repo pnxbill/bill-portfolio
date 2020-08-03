c6aeac4aff6c106d523584669d7f048b0d1cfb87
Bill Marques <pnxbill@gmail.com>
2020-08-02 21:15:44 -0300

    fix: extracted login form from component

--- components/forms/LoginForm.js
+++ components/forms/LoginForm.js
@@ -7,56 +7,20 @@ const LoginForm = ({ onSubmit }) => {
   const { register, handleSubmit } = useForm();
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="form-group">
-        <label htmlFor="avatar">Avatar</label>
-        <input
-          ref={register}
-          type="text"
-          className="form-control"
-          name="avatar"
-          id="avatar"
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="username">Username</label>
-        <input
-          ref={register}
-          type="text"
-          className="form-control"
-          name="username"
-          id="username"
-        />
-      </div>
+    <form>
       <div className="form-group">
         <label htmlFor="email">Email</label>
         <input
-          ref={register}
           type="email"
           className="form-control"
-          name="email"
-          id="email"
-        />
+          id="email" />
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
         <input
-          ref={register}
           type="password"
           className="form-control"
-          name="password"
-          id="password"
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="passwordConfirmation">Password Confirmation</label>
-        <input
-          ref={register}
-          type="password"
-          className="form-control"
-          name="passwordConfirmation"
-          id="passwordConfirmation"
-        />
+          id="password" />
       </div>
       <button
         type="submit"
