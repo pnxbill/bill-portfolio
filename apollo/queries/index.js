64459ca1c2133977d8e61f8b1d99a265153e5c17
Bill Marques <pnxbill@gmail.com>
2020-08-03 22:29:59 -0300

    feat: Created sign in graphql query

--- apollo/queries/index.js
+++ apollo/queries/index.js
@@ -119,4 +119,21 @@ export const SIGN_UP = gql`
   }
 `;
 
+export const SIGN_IN = gql`
+  mutation SignIn(
+    $email: String!
+    $password: String!
+  ) {
+    signIn(input: {
+      email: $email
+      password: $password
+    }) {
+      _id
+      username
+      role
+      avatar
+    }
+  }
+`;
+
 // END AUTH QUERIES
