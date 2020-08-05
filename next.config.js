6b6e2e748d4bd2c67a0acbea7c822222ada13bdc
Bill Marques <pnxbill@gmail.com>
2020-08-04 21:55:16 -0300

    fix: fixed an issue in next config to use @ as root folder

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
   webpack: (config) => {
 
     config.resolve.alias['@'] = path.resolve(__dirname);
+    console.log('teste', path_resolve(__dirname))
     return config;
   }
 }
