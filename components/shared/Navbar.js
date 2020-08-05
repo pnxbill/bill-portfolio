087d65e3e012f221e329b91b2d0d414c0af267e8
Bill Marques <pnxbill@gmail.com>
2020-08-04 21:55:38 -0300

    feat: getting user authenticated in navbar and showing message when he has a session

--- components/shared/Navbar.js
+++ components/shared/Navbar.js
@@ -1,6 +1,9 @@
 
 import { Navbar, Nav } from 'react-bootstrap';
 import Link from 'next/link';
+import withApollo from '@/hoc/withApollo'
+import { useState, useEffect } from 'react';
+import { useLazyGetUser } from '../../apollo/actions';
 
 const AppLink = ({ children, className, href }) =>
   <Link href={href}>
@@ -8,6 +11,18 @@ const AppLink = ({ children, className, href }) =>
   </Link>
 
 const AppNavbar = () => {
+  const [user, setUser] = useState(null);
+  const [hasResponse, setHasResponse] = useState(null);
+  const [getUser, { data, error }] = useLazyGetUser();
+
+  useEffect(() => {
+    getUser();
+  }, [])
+
+  if (data && !hasResponse) {
+    if (data.user && !user) setUser(data.user)
+    setHasResponse(true);
+  }
 
   return (
     <div className="navbar-wrapper">
@@ -24,19 +39,31 @@ const AppNavbar = () => {
             <AppLink href="/forum/categories" className="nav-link">Forum</AppLink>
             <AppLink href="/cv" className="nav-link">CV</AppLink>
           </Nav>
-          <Nav>
-            <AppLink href="/login" className="nav-link">Sign In</AppLink>
-            <AppLink
-              href="/register"
-              className="nav-link btn btn-success bg-green-2 bright"
-            >
-              Sign Up
-            </AppLink>
-          </Nav>
+          {hasResponse &&
+            <Nav>
+              {user &&
+                <>
+                  <span className="nav-link mr-4">Welcome {user.username}</span>
+                  <AppLink href="/login" className="nav-link btn btn-danger">Sign Out</AppLink>
+                </>
+              }
+              {(error || !user) &&
+                <>
+                  <AppLink href="/login" className="nav-link">Sign In</AppLink>
+                  <AppLink
+                    href="/register"
+                    className="nav-link btn btn-success bg-green-2 bright"
+                  >
+                    Sign Up
+                  </AppLink>
+                </>
+              }
+            </Nav>
+          }
         </Navbar.Collapse>
       </Navbar>
     </div>
   )
 }
 
-export default AppNavbar;
+export default withApollo(AppNavbar);
