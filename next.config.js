69562eff7d067a754d196fba90f5dd370b376a4e
Bill Marques <pnxbill@gmail.com>
2020-08-05 22:03:48 -0300

    fix: bug in next config that wouldnt let server load

--- next.config.js
+++ next.config.js
@@ -5,9 +5,7 @@ const path = require('path');
 // Configuration for easy imports with @ meaning root folder
 module.exports = {
   webpack: (config) => {
-
     config.resolve.alias['@'] = path.resolve(__dirname);
-    console.log('teste', path_resolve(__dirname))
     return config;
   }
 }
