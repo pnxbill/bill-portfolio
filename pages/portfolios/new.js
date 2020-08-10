14436981e89d9200e390554eafc033aae920d0ac
Bill Marques <pnxbill@gmail.com>
2020-08-09 22:31:03 -0300

    feat: redirect to portfolios after creating one

--- pages/portfolios/new.js
+++ pages/portfolios/new.js
@@ -2,15 +2,22 @@
 import PortfolioForm from '@/components/forms/PortfolioForm';
 import withApollo from '@/hoc/withApollo';
 import withAuth from '@/hoc/withAuth';
+import { useRouter } from 'next/router';
 import { useCreatePortfolio } from '../../apollo/actions';
 
 
 const PortfolioNew = () => {
-
+  const router = useRouter();
   const [createPortfolio, { error }] = useCreatePortfolio();
 
   const errorMessage = (err) => {
     return err.graphQLErrors[0]?.message || "Oooooops, something went wrong..."
+  }
+
+  const handleSubmit = async (data) => {
+    await createPortfolio({ variables: data });
+    router.push('/portfolios')
+
   }
 
 
@@ -20,7 +27,7 @@ const PortfolioNew = () => {
         <div className="row">
           <div className="col-md-5 mx-auto">
             <h1 className="page-title">Create New Portfolio</h1>
-            <PortfolioForm onSubmit={data => createPortfolio({ variables: data })} />
+            <PortfolioForm onSubmit={handleSubmit} />
             {error && <div className="alert alert-danger">{errorMessage(error)}</div>}
           </div>
         </div>
