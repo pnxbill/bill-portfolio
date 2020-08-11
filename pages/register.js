bd37912fa95a254cab9f508cf5edf840c925e95b
Bill Marques <pnxbill@gmail.com>
2020-08-10 22:50:36 -0300

    chore: applied base layout to register page

--- pages/register.js
+++ pages/register.js
@@ -3,6 +3,7 @@ import { Mutation } from 'react-apollo';
 import { SIGN_UP } from '@/apollo/queries';
 import withApollo from '@/hoc/withApollo';
 import Redirect from '../components/shared/Redirect';
+import BaseLayout from '@/layouts/BaseLayout';
 
 const Register = () => {
 
@@ -11,7 +12,7 @@ const Register = () => {
   }
 
   return (
-    <>
+    <BaseLayout>
       <div className="bwm-form mt-5">
         <div className="row">
           <div className="col-md-5 mx-auto">
@@ -32,7 +33,7 @@ const Register = () => {
           </div>
         </div>
       </div>
-    </>
+    </BaseLayout>
   )
 }
 
