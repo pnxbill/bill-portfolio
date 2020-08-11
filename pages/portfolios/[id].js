ae2c820db42900a0e0612266171ec02b4e3465e4
Bill Marques <pnxbill@gmail.com>
2020-08-10 22:52:50 -0300

    chore: applied base layout to specific portfolio page

--- pages/portfolios/[id].js
+++ pages/portfolios/[id].js
@@ -4,6 +4,7 @@ import { GET_PORTFOLIO } from '../../apollo/queries';
 import { useRouter } from 'next/router';
 import withApollo from '@/hoc/withApollo';
 import { getDataFromTree } from '@apollo/react-ssr';
+import BaseLayout from '@/layouts/BaseLayout';
 
 
 const PortfolioDetail = () => {
@@ -14,43 +15,45 @@ const PortfolioDetail = () => {
 
   const { title, jobTitle, companyWebsite, startDate, endDate, description } = portfolio;
   return (
-    <div className="portfolio-detail">
-      <div className="container">
+    <BaseLayout>
+      <div className="portfolio-detail">
+        <div className="container">
 
-        <div className="jumbotron">
-          <h1 className="display-3">{title}</h1>
-          <p className="lead">{jobTitle}</p>
-          <p>
-            <a className="btn btn-lg btn-success" href={companyWebsite} role="button">
-              See Company</a>
-          </p>
-        </div>
-
-        <div className="row marketing">
-          <div className="col-lg-6">
-            <h4 className="title">Location</h4>
-            <p className="text">Some Location</p>
-
-            <h4 className="title">Start Date</h4>
-            <p className="text">{startDate}</p>
+          <div className="jumbotron">
+            <h1 className="display-3">{title}</h1>
+            <p className="lead">{jobTitle}</p>
+            <p>
+              <a className="btn btn-lg btn-success" href={companyWebsite} role="button">
+                See Company</a>
+            </p>
           </div>
 
-          <div className="col-lg-6">
-            {/* TODO: days later... */}
-            <h4 className="title">Days</h4>
-            <p className="text">44</p>
+          <div className="row marketing">
+            <div className="col-lg-6">
+              <h4 className="title">Location</h4>
+              <p className="text">Some Location</p>
 
-            <h4 className="title">End Date</h4>
-            <p className="text">{endDate}</p>
-          </div>
-          <div className="col-md-12">
-            <hr />
-            <h4 className="title">Description</h4>
-            <p>{description}</p>
+              <h4 className="title">Start Date</h4>
+              <p className="text">{startDate}</p>
+            </div>
+
+            <div className="col-lg-6">
+              {/* TODO: days later... */}
+              <h4 className="title">Days</h4>
+              <p className="text">44</p>
+
+              <h4 className="title">End Date</h4>
+              <p className="text">{endDate}</p>
+            </div>
+            <div className="col-md-12">
+              <hr />
+              <h4 className="title">Description</h4>
+              <p>{description}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </BaseLayout>
   )
 }
 
