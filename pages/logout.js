ac96bde5f08e5140c64d57ce9b3744a5a4b2332c
Bill Marques <pnxbill@gmail.com>
2020-08-10 22:53:14 -0300

    chore: applied base layout to logout page

--- pages/logout.js
+++ pages/logout.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import withApollo from '@/hoc/withApollo';
 import { useSignOut } from '../apollo/actions';
+import BaseLayout from '@/layouts/BaseLayout';
 
 const Logout = ({ apollo }) => { // Called apollo from withApollo
   const [signOut] = useSignOut();
@@ -14,7 +15,7 @@ const Logout = ({ apollo }) => { // Called apollo from withApollo
   }, []);
 
   return (
-    <>
+    <BaseLayout>
       <div className="bwm-form mt-5">
         <div className="row">
           <div className="col-md-5 mx-auto">
@@ -23,7 +24,7 @@ const Logout = ({ apollo }) => { // Called apollo from withApollo
           </div>
         </div>
       </div>
-    </>
+    </BaseLayout>
   )
 }
 
