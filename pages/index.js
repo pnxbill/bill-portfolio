4c46b0a8aae5b33c609efbad6f49cece04dfc763
Bill Marques <pnxbill@gmail.com>
2020-08-13 23:37:58 -0300

    changing the card context to fix layout bug

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ const Home = () => (
             <div className="card-body">
               <h5 className="card-title">Card title</h5>
               <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p className="card-text fs-2 ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p className="card-text fs-2 ">changing the card context to fix layout bug</p>
             </div>
             <div className="card-footer no-border">
               <small className="text-muted">Last updated 3 mins ago</small>
