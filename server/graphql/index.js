1350adcaecb9e4f9a75cdb61ebec250476201fcb
Bill Marques <pnxbill@gmail.com>
2020-08-14 23:37:11 -0300

    chore: created new query for get portfolios by user on root query apollo

--- server/graphql/index.js
+++ server/graphql/index.js
@@ -17,6 +17,7 @@ exports.createApolloServer = () => {
     type Query {
       portfolio(id: ID): Portfolio
       portfolios: [Portfolio]
+      userPortfolios: [Portfolio]
 
       user: User
     }
