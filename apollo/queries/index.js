b1629d3584cd21a3a280e173acb02c116dbbb438
Bill Marques <pnxbill@gmail.com>
2020-09-12 23:03:34 -0300

    chore: removing static data from updatePortfolio query and using variables

--- apollo/queries/index.js
+++ apollo/queries/index.js
@@ -81,16 +81,26 @@ export const CREATE_PORTFOLIO = gql`
 `;
 
 export const UPDATE_PORTFOLIO = gql`
-  mutation UpdatePortfolio($id: ID) {
+  mutation UpdatePortfolio(
+    $id: ID
+    $title: String
+    $company: String
+    $companyWebsite: String
+    $location: String
+    $jobTitle: String
+    $description: String
+    $startDate: String
+    $endDate: String
+  ) {
     updatePortfolio(id: $id, input: {
-      title: "JOB FLORIPA 2"
-      company: "WAVECODE EIRELLI"
-      companyWebsite: "https://wavecode.com.br"
-      location: "São José, SC"
-      jobTitle: "Programador"
-      description: "Inserir código no teclado"
-      startDate: "2012-12-12T23:59Z"
-      endDate: "2013-11-14T23:59Z"
+      title: $title
+      company: $company
+      companyWebsite: $companyWebsite
+      location: $location 
+      jobTitle: $jobTitle 
+      description: $description 
+      startDate: $startDate
+      endDate: $endDate
     }) {
       _id
       title
