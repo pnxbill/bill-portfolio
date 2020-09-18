39f3439af679ade7db721028bf46ef3d33e80009
Bill Marques <pnxbill@gmail.com>
2020-09-17 23:12:47 -0300

    feat: added unique-slug package to handle topics with same slug"

--- server/graphql/models/Topic.js
+++ server/graphql/models/Topic.js
@@ -1,5 +1,6 @@
 
 const slugify = require('slugify');
+const uniqueSlug = require('unique-slug');
 
 class Topic {
   constructor(model, user) {
@@ -14,15 +15,29 @@ class Topic {
       .populate('forumCategory');
   }
 
+  async _create(data) {
+    const createdTopic = await this.Model.create(data);
+    return this.Model.findById(createdTopic._id).populate('user').populate('forumCategory')
+  }
+
   async create(data) {
     if (!this.user) throw new Error('You need to authenticate in order to create a topic!');
     data.user = this.user;
     // Generate slug
     data.slug = slugify(data.title, { lower: true });
 
-    const createdTopic = await this.Model.create(data);
+    try {
+      const topic = await this._create(data);
+      return topic;
+    } catch (err) {
+      if (err.code == 11000 && err.keyPattern && err.keyPattern.slug) {
+        data.slug += `-${uniqueSlug()}`
+        const topic = await this._create(data);
+        return topic;
+      }
+      return null;
+    }
 
-    return this.Model.findById(createdTopic._id).populate('user').populate('forumCategory')
   }
 }
 
