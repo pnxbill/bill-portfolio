8da69011224f3de0c3b9a1eb9332c357d06afd5e
Bill Marques <pnxbill@gmail.com>
2020-09-29 00:00:20 -0300

    feat: only if user logged in he can create topic

--- pages/forum/categories/[slug].js
+++ pages/forum/categories/[slug].js
@@ -1,12 +1,17 @@
 import BaseLayout from '@/layouts/BaseLayout';
-import { useGetTopicsByCategory } from '@/apollo/actions';
+import { useGetTopicsByCategory, useGetUser } from '@/apollo/actions';
 import { useRouter } from 'next/router';
 import withApollo from '../../../hoc/withApollo';
 import { getDataFromTree } from '@apollo/react-ssr';
+import Replier from '@/components/shared/Replier';
+import { useState } from 'react';
 
 const Topics = () => {
+  const [isReplierOpen, setReplierOpen] = useState(false);
   const { slug } = useRouter().query;
   const { data } = useGetTopicsByCategory({ variables: { slug } });
+  const { data: dataUser } = useGetUser();
+  const user = dataUser?.user;
 
   const topicsByCategory = (data && data.topicsByCategory) || [];
 
@@ -16,6 +21,11 @@ const Topics = () => {
         <div className="px-2">
           <div className="pt-5 pb-4">
             <h1>Select a topic</h1>
+            {user &&
+              <button className="btn btn-primary" onClick={() => setReplierOpen(true)}>
+                Create Topic
+              </button>
+            }
           </div>
         </div>
       </section>
@@ -39,44 +49,8 @@ const Topics = () => {
             ))}
           </tbody>
         </table>
-        <div className='reply-controls'>
-          <div className="reply-area">
-            <div className="reply-to">
-              Reply To: <span className="text ml-2">User 1</span>
-            </div>
-            <div className="fj-editor-input">
-              <input
-                name="title"
-                placeholder="Topic title"
-                type="text"></input>
-            </div>
-            <div className="fj-editor">
-              <div className="fj-editor-textarea-wrapper">
-                <textarea
-                  name="content"
-                  placeholder="Type here">
-                </textarea>
-              </div>
-              <div className="fj-editor-preview-wrapper">
-                <div className="preview">
-                  <p></p>
-                </div>
-              </div>
-            </div>
-            <div className="submit-area">
-              <div className="send mr-auto">
-                <button
-                  href="#"
-                  className="btn btn-main bg-blue py-2 ttu">Reply</button>
-                <a className="btn py-2 ttu gray-10">Cancel</a>
-              </div>
-              <div>
-                <a className="btn py-2 ttu gray-10">hide preview</a>
-              </div>
-            </div>
-          </div>
-        </div>
       </section>
+      <Replier isOpen={isReplierOpen} />
     </BaseLayout>
   )
 }
