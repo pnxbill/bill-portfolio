ad262c3fd6c0bf923d25c6ff8b447ae6f05d1d11
Bill Marques <pnxbill@gmail.com>
2020-10-05 23:51:07 -0300

    populating response from topics by slug with user/forumcategory

--- server/graphql/models/Topic.js
+++ server/graphql/models/Topic.js
@@ -10,7 +10,7 @@ class Topic {
 
 
   getBySlug(slug) {
-    return this.Model.findOne({ slug });
+    return this.Model.findOne({ slug }).populate('user').populate('forumCategory');
   }
 
   getAllByCategory(forumCategory) {
