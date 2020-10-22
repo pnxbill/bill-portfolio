60daa8e180e37d764b7102d8715146a0556cc9b1
Bill Marques <pnxbill@gmail.com>
2020-10-21 23:24:13 -0300

    feat: created format date from now function

--- utils/functions.js
+++ utils/functions.js
@@ -3,3 +3,5 @@ import moment from 'moment';
 export const formatDate = date => {
   return date ? moment.unix(date / 1000).format('DD/MM/YYYY') : '';
 }
+
+export const fromNow = date => moment.unix(date / 1000).fromNow();
