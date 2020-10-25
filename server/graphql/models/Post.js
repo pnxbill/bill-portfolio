955a982a96c1cb9962f6728da107d0a9cf48e90e
Bill Marques <pnxbill@gmail.com>
2020-10-24 21:27:10 -0300

    feat: created logic inside Post model to create post

--- server/graphql/models/Post.js
+++ server/graphql/models/Post.js
@@ -1,3 +1,5 @@
+const moment = require('moment');
+const uniqueSlug = require('unique-slug');
 
 class Post {
   constructor(model, user) {
@@ -11,6 +13,32 @@ class Post {
       .sort('createdAt')
       .populate('topic')
       .populate('user')
+      .populate({ path: 'parent', populate: 'user' })
+  };
+
+  async create(post) {
+    if (!this.user) {
+      throw new Error('You must be signed in to create a post!')
+    }
+    post.user = this.user;
+
+    const createdAt = moment().toISOString();
+    const slugPart = uniqueSlug();
+    const fullSlugPart = createdAt + ':' + slugPart;
+
+    if (post.parent) {
+      const parent = await this.Model.findById(post.parent);
+      post.slug = parent.slug + '/' + slugPart;
+      post.fullSlugPart = parent.fullSlugPart + '/' + fullSlugPart;
+    } else {
+      post.slug = slugPart;
+      post.fullSlugPart = fullSlugPart;
+    }
+
+    const createdPost = await this.Model.create(post);
+    return this.Model
+      .findById(createdPost._id)
+      .populate('topic')
       .populate({ path: 'parent', populate: 'user' })
   }
 }
